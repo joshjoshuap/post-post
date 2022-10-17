ddb21ab0d96ba090b007c04b3fd78ca551b69def
Joshua <joshjoshuap1@gmail.com>
2022-10-17 17:13:05 +0800

    signup user test

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,3 +1,7 @@
+const bcrypt = require("bcryptjs");
+
+const UserModel = require("../models/user-model");
+
 // GET: /api/user/
 exports.getUsers = (req, res, next) => {
   res.send("Users");
@@ -9,6 +13,39 @@ exports.postLogin = (req, res, next) => {
 };
 
 // POST: /api/user/signup
-exports.postSignup = (req, res, next) => {
-  res.send("Signup");
+exports.postSignup = async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  let existingUser;
+  try {
+    existingUser = await UserModel.findOne({ email: email });
+  } catch (err) {
+    console.log("Signup Failed", err);
+  }
+
+  if (existingUser) {
+    console.log("Email Exist");
+  }
+
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    console.log("Hashing Password Filed", err);
+  }
+
+  const createUser = new UserModel({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await createUser.save();
+    console.log("Signup Success");
+  } catch (err) {
+    console.log("Signup Failed", err);
+  }
+
+  res.status(201).json({ user: createUser.toObject({ getters: true }) });
 };

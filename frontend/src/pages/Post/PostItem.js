67f102de03d5b2c40fe0861e8488ba64ea8d8c29
Joshua <joshjoshuap1@gmail.com>
2022-10-21 14:38:43 +0800

    chore: implement frontend delete

--- frontend/src/pages/Post/PostItem.js
+++ frontend/src/pages/Post/PostItem.js
@@ -1,6 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const PostItem = (props) => {
+  const apiBackendUrl = process.env.REACT_APP_BACKEND_URL;
+  const navigate = useNavigate();
+  const postId = props.id;
+
+  const formSubmitHandler = async (event) => {
+    event.preventDefault();
+    try {
+      const res = await fetch(`${apiBackendUrl}/api/post/${postId}`, {
+        method: "DELETE",
+      });
+
+      const data = await res.json();
+
+      // if response is not ok or fetch failed
+      if (!res.ok) {
+        throw new Error(data.message);
+      }
+
+      navigate("/");
+    } catch (err) {
+      console.log("Delete falied", err);
+    }
+  };
+
   return (
     <div>
       <h1>{props.title}</h1>
@@ -11,6 +35,9 @@ const PostItem = (props) => {
       <div>
         <Link to={`/post/${props.id}`}>Edit</Link>
       </div>
+      <form onSubmit={formSubmitHandler}>
+        <button type="submit">Delete</button>
+      </form>
     </div>
   );
 };
